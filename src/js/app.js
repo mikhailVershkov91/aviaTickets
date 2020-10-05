7a7c0508b3ab7cb32456e698e25a5fcfb9c41ea7
mikhailVershkov91 <mikhail.vershkov1991@gmail.com>
2020-10-05 14:22:23 +0600

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../css/style.css';
 import './plugins';
 import locations from './store/locations';
 import formUI from './views/form';
+import ticketsUI from './views/tickets'
 import currencyUI from './views/currency';
 
 document.addEventListener('DOMContentLoaded', e => {
@@ -36,5 +37,6 @@ document.addEventListener('DOMContentLoaded', e => {
     });
 
     console.log(locations.lastSearch);
+    ticketsUI.renderTickets(locations.lastSearch);
   }
 });
